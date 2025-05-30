38b431cd50e88b59a8ab94acb121a2b20a78a8bf
bilalm354 <bilalm354@gmail.com>
2025-05-30 14:46:31 -0400

    make cube

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
